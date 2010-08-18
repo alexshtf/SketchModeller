f0ea400dca443fd425b773a20732a4c644ac1520
unknown <Alex@.(none)>
2010-08-18 11:52:30 +0300

    Initial import of SkelToCyl

---
+++ SkelToCyl/SkelToCyl/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics.Contracts;
+using System.Windows.Media.Media3D;
+
+namespace SkelToCyl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var skeleton = GenerateSkeleton();
+            var mesh = SkeletonToMesh.SkeletonToCylinder(skeleton, 30);
+
+            model.Points = new Point3DCollection(mesh.Item1);
+            //model.Points = new Point3DCollection(
+            //    from item in skeleton
+            //    select item.Position);
+        }
+
+        private IEnumerable<SkeletonPoint> GenerateSkeleton()
+        {
+            foreach (var t in LinSpace(0, 2 * Math.PI, 30))
+            {
+                yield return new SkeletonPoint
+                {
+                    Position = new Point3D(5 * Math.Cos(t), 0.5 * t, 5 * Math.Sin(t)),
+                    Normal = new Vector3D(-5 * Math.Sin(t), 2, 5 * Math.Cos(t)),
+                    Radius = 0.5 + 0.1 * Math.Cos(2 * t),
+                };
+            }
+        }
+
+        private IEnumerable<double> LinSpace(double min, double max, int nSteps)
+        {
+            Contract.Requires(nSteps >= 2);
+            Contract.Requires(max > min);
+
+            Contract.Ensures(Contract.Result<IEnumerable<double>>().Count() == nSteps);
+            Contract.Ensures(Contract.Result<IEnumerable<double>>().First() == min);
+            Contract.Ensures(Contract.Result<IEnumerable<double>>().Last() == max);
+
+            return LinSpaceCore(min, max, nSteps);
+        }
+
+        private IEnumerable<double> LinSpaceCore(double min, double max, int nSteps)
+        {
+            for (int i = 0; i < nSteps; ++i)
+            {
+                var current = min + i * (max - min) / (nSteps - 1);
+                yield return current;
+            }
+        }
+    }
+}
---
+++ SkelToCyl/SkelToCyl/SkeletonToMesh.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+using System.Diagnostics.Contracts;
+
+namespace SkelToCyl
+{
+    static class SkeletonToMesh
+    {
+        /// <summary>
+        /// Constructs a mesh of a cylinder given its skeleton.
+        /// </summary>
+        /// <param name="skeleton">The 1D skeleton of the cylinder</param>
+        /// <returns></returns>
+        public static Tuple<Point3D[], Vector3D[], int[]> SkeletonToCylinder(IEnumerable<SkeletonPoint> skeleton, int slices)
+        {
+            List<Point3D> positions = new List<Point3D>();
+            List<int> indices = new List<int>();
+
+            Vector3D u = GetVectorInFirstPlane(skeleton);
+            foreach (var pair in skeleton.ZipIndex())
+            {
+                // find orthonormal basis for the plane at skelPoint
+                u = ProjectOnPlane(u, Normalized(skelPoint.Normal), skelPoint.Position);
+                Vector3D v = Vector3D.CrossProduct(u, skelPoint.Normal);
+                u.Normalize();
+                v.Normalize();
+
+                // create a circle around the skeleton point
+                for (int i = 0; i < slices; ++i)
+                {
+                    var angle = 2 * i * Math.PI / slices;
+                    var x = skelPoint.Radius * Math.Cos(angle);
+                    var y = skelPoint.Radius * Math.Sin(angle);
+
+                    var pnt = skelPoint.Position + x * u + y * v; // create a point in the plane, using its orthonormal basis
+                    positions.Add(pnt);
+                }
+            }
+
+            Vector3D[] normals = null;
+            int[] indices = null;
+            return Tuple.Create(positions.ToArray(), normals, indices);
+        }
+
+        private static Vector3D Normalized(Vector3D input)
+        {
+            input.Normalize();
+            return input;
+        }
+
+        private static Vector3D GetVectorInFirstPlane(IEnumerable<SkeletonPoint> skeleton)
+        {
+            var first = skeleton.FirstOrDefault();
+            return FindVectorInPlane(first.Position, first.Normal);
+        }
+
+        private static Vector3D FindVectorInPlane(Point3D pnt, Vector3D normal)
+        {
+            normal.Normalize();
+
+            // find a vector not parallel to the normal
+            var vec = normal;
+            vec += new Vector3D(1, -1, 1);
+
+            return ProjectOnPlane(vec, normal, pnt);
+        }
+
+        private static Vector3D ProjectOnPlane(Vector3D toProject, Vector3D normal, Point3D pnt)
+        {
+            var p = pnt + toProject;
+            var proj = ProjectOnPlane(p, normal, pnt);
+            return proj - pnt;
+        }
+
+        [Pure]
+        private static double Substitute(Point3D toSubst, Vector3D normal, Point3D pnt)
+        {
+            return Vector3D.DotProduct((toSubst - pnt), normal);
+        }
+
+        private static Point3D ProjectOnPlane(Point3D toProject, Vector3D normal, Point3D pnt)
+        {
+            Contract.Requires(Math.Abs(normal.LengthSquared - 1) <= 1E-5); // normal is normalized and non-degenerate                        
+            Contract.Ensures(Math.Abs(Substitute(Contract.Result<Point3D>(), normal, pnt)) <= 1E-5); // result on the plane
+
+            var u = Vector3D.DotProduct(normal, toProject - pnt);
+            var proj = new Point3D(
+                toProject.X - normal.X * u,
+                toProject.Y - normal.Y * u,
+                toProject.Z - normal.Z * u);
+
+            return proj;
+        }
+    }
+}
