7eccb1ee99c4bc01195369485233ae408e72b5c2
Alex <alex.shtf@gmail.com>
2010-10-27 09:54:42 +0200

    Move based classes to Base/ subdirectory

---
+++ MultiviewCurvesToCyl/MultiviewCurvesToCyl/Base/Base3DViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utils;
+
+namespace MultiviewCurvesToCyl.Base
+{
+    class Base3DViewModel : BaseViewModel, I3DViewModel
+    {
+        #region IsInWireframeMode property
+
+        private bool isInWireframeMode;
+
+        public bool IsInWireframeMode
+        {
+            get { return isInWireframeMode; }
+            set
+            {
+                var oldVal = isInWireframeMode;
+                isInWireframeMode = value;
+                OnIsInWireframeModeChanged(oldVal, value);
+            }
+        }
+
+        protected virtual void OnIsInWireframeModeChanged(bool oldVal, bool newVal)
+        {
+            NotifyPropertyChanged(() => IsInWireframeMode);
+        }
+
+        #endregion
+    }
+}
---
+++ MultiviewCurvesToCyl/MultiviewCurvesToCyl/Base/I3DViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiviewCurvesToCyl.Base
+{
+    interface I3DViewModel
+    {
+        bool IsInWireframeMode { get; set; }
+    }
+}
