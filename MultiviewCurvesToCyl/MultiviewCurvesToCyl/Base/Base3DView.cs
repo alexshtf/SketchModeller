146178d3bc18601429f7f94e27aa9edeaa9188a6
Alex <alex.shtf@gmail.com>
2010-10-27 10:43:14 +0200

    Added Base3DView class and IBase3DView interface

---
+++ MultiviewCurvesToCyl/MultiviewCurvesToCyl/Base/Base3DView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MultiviewCurvesToCyl.Controls;
+using System.Windows.Media.Media3D;
+
+namespace MultiviewCurvesToCyl.Base
+{
+    class Base3DView : IVisual3DFactory
+    {
+        public Visual3D Create(object item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MultiviewCurvesToCyl/MultiviewCurvesToCyl/Base/I3DViewModel.cs
+++ MultiviewCurvesToCyl/MultiviewCurvesToCyl/Base/I3DViewModel.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace MultiviewCurvesToCyl.Base
 {
-    interface I3DViewModel
+    interface I3DViewModel : INotifyPropertyChanged
     {
         bool IsInWireframeMode { get; set; }
     }
