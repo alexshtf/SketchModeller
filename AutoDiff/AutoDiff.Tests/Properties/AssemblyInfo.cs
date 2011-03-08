5b9da489aaf9a11b98efd7d980e354e71b464144
Alex <alex.shtf@gmail.com>
2011-03-08 15:34:10 +0200

    Forgot to add the unit-tests project and unit-test config files.

--- AutoDiff/AutoDiff.Tests/Properties/AssemblyInfo.cs
+++ AutoDiff/AutoDiff.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoDiffTest")]
+[assembly: AssemblyTitle("AutoDiff.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NA")]
-[assembly: AssemblyProduct("AutoDiffTest")]
-[assembly: AssemblyCopyright("Copyright © NA 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoDiff.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c25ed592-d8f8-4130-88ce-301eb88a9d50")]
+[assembly: Guid("4d4b3381-a113-4449-99f2-7059a66e57fc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
