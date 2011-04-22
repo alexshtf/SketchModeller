27143a3c6adac937d15cd822bbc717ad91c1631f
Alex <alex.shtf@gmail.com>
2011-04-22 16:01:34 +0300

    Fixed a problem with the tap sketch

---
+++ SketchModeller/SketchModeller.Utilities/DebugPrint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SketchModeller.Utilities
+{
+    public static class DebugPrint
+    {
+        public static string ToMatlab(this Tuple<Point, Point> pts)
+        {
+            var ptsArray = new Point[] { pts.Item1, pts.Item2 };
+            return ToMatlab(ptsArray);
+        }
+
+        private static string ToMatlab(this IEnumerable<Point> points)
+        {
+            if (!points.Any())
+                return "[]";
+
+            const string FORMAT_FIRST = "{0}, {1};";
+            const string FORMAT_REST = "\n  {0}, {1};";
+            
+            // put the first point
+            var sb = new StringBuilder("[ ");
+            sb.AppendFormat(FORMAT_FIRST, points.First().X, points.First().Y);
+            
+            // put the rest of the points
+            foreach (var pnt in points.Skip(1))
+                sb.AppendFormat(FORMAT_REST, pnt.X, pnt.Y);
+
+            // remove the ';' character at the end
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append(" ]");
+
+            return sb.ToString();
+        }
+    }
+}
