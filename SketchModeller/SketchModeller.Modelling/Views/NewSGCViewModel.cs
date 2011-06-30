70d61a096e8ad51dacb38025db93e4fff42d6748
Alex <alex.shtf@gmail.com>
2011-06-30 10:05:18 +0300

    Added new straight generalized cylinder view-model

---
+++ SketchModeller/SketchModeller.Modelling/Views/NewSGCViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SketchModeller.Infrastructure.Shared;
+using SketchModeller.Infrastructure.Services;
+using Microsoft.Practices.Prism.Events;
+using SketchModeller.Infrastructure.Data;
+
+namespace SketchModeller.Modelling.Views
+{
+    class NewSGCViewModel : NewPrimitiveViewModel
+    {
+        private NewStraightGenCylinder model;
+
+        public NewSGCViewModel(
+            UiState uiState = null, 
+            ICurveAssigner curveAssigner = null, 
+            IEventAggregator eventAggregator = null)
+            : base(uiState, curveAssigner, eventAggregator)
+        {
+            model = new NewStraightGenCylinder;
+        }
+    }
+}
