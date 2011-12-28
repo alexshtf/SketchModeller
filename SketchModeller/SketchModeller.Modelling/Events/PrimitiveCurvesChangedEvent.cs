03043ee6d2da9bacaa758303e10e8bf501b3fd4a
Alex <alex.shtf@gmail.com>
2011-12-28 13:14:15 +0200

    Title changes + added documentation
    - Added default title to the shell window
    - Added documentation to the abstract methods of NewPrimitiveViewModel
    - Added documentation to the class PrimitiveCurvesChangedEvent

--- SketchModeller/SketchModeller.Modelling/Events/PrimitiveCurvesChangedEvent.cs
+++ SketchModeller/SketchModeller.Modelling/Events/PrimitiveCurvesChangedEvent.cs
@@ -8,7 +8,7 @@ using SketchModeller.Infrastructure.Data;
 namespace SketchModeller.Modelling.Events
 {
     /// <summary>
-    /// Notifies that the data of primitive curves has changed.
+    /// Notifies that the data of new primitive curves has changed.
     /// </summary>
     class PrimitiveCurvesChangedEvent : CompositePresentationEvent<NewPrimitive>
     {
