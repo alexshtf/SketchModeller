3b312e7d27a98ea5f5e938e9b5dc42640d6632d7
Alex <alex.shtf@gmail.com>
2012-01-01 16:52:02 +0200

    Added collinear centers inference capability (might not work)

--- SketchModeller/SketchModeller.Modelling/Services/AnnotationInference/ColinearCentersInferer.cs
+++ SketchModeller/SketchModeller.Modelling/Services/AnnotationInference/ColinearCentersInferer.cs
@@ -8,31 +8,48 @@ using Utils;
 
 using Enumerable = System.Linq.Enumerable;
 using UtilsEnumerable = Utils.Enumerable;
+using System.Windows.Media.Media3D;
 
 namespace SketchModeller.Modelling.Services.AnnotationInference
 {
     class ColinearCentersInferer : IInferrer
     {
-        private SessionData sessionData;
+        private const double DEFAULT_PROXIMITY_THRESHOLD = 0.6;
+        private const double DEFAULT_COLINEARITY_THRESHOLD = 10 * Math.PI / 180; // 10 degrees angle to be considered "colinear"
+        private const double DEFAULT_PARALLELISM_THRSHOLD = 5 * Math.PI / 180; // 5 degrees angle between vectors to be considered "parallel"
 
-        public ColinearCentersInferer(SessionData sessionData)
+        private SessionData sessionData;
+        private double proximityThreshold;
+        private double colinearityThreshold;
+        private double parallelismThreshold;
+
+        public ColinearCentersInferer(SessionData sessionData, 
+                                      double proximityThreshold = DEFAULT_PROXIMITY_THRESHOLD,
+                                      double colinearityThreshold = DEFAULT_COLINEARITY_THRESHOLD,
+                                      double parallelismThreshold = DEFAULT_PARALLELISM_THRSHOLD)
         {
             this.sessionData = sessionData;
+            this.proximityThreshold = proximityThreshold;
+            this.colinearityThreshold = colinearityThreshold;
+            this.parallelismThreshold = parallelismThreshold;
         }
 
         public IEnumerable<Annotation> InferAnnotations(NewPrimitive toBeSnapped, SnappedPrimitive toBeAnnotated)
         {
-            return Enumerable.Empty<Annotation>();
             var toBeAnnotatedCurves = toBeAnnotated.FeatureCurves;
             var candidateTriples =
                 from i in Enumerable.Range(0, toBeAnnotatedCurves.Length)
                 from j in Enumerable.Range(i + 1, toBeAnnotatedCurves.Length - i - 1)
-                let firstNewCurve = toBeAnnotatedCurves[i]
-                let secondNewCurve = toBeAnnotatedCurves[j]
+                // at this point (i, j) are all the possible pairs of curves without repetitions
                 let allExistingCurves = sessionData.FeatureCurves.Except(toBeAnnotatedCurves)
                 from existingCurve in allExistingCurves
-                where AreGoodCandidates(firstNewCurve, secondNewCurve, existingCurve)
-                select new { FistNewCurve = firstNewCurve, SecondNewCurve = secondNewCurve, ExistingCurve = existingCurve };
+                where AreGoodCandidates(toBeAnnotatedCurves[i], toBeAnnotatedCurves[j], existingCurve)
+                select new 
+                { 
+                    FistNewCurve = toBeAnnotatedCurves[i], 
+                    SecondNewCurve = toBeAnnotatedCurves[j], 
+                    ExistingCurve = existingCurve 
+                };
 
             if (candidateTriples.Any())
             {
@@ -49,12 +66,59 @@ namespace SketchModeller.Modelling.Services.AnnotationInference
 
         private bool AreGoodCandidates(FeatureCurve firstNewCurve, FeatureCurve secondNewCurve, FeatureCurve existingCurve)
         {
-            throw new NotImplementedException();
+            bool areAlmostColinear = AreAlmostColinear(firstNewCurve, secondNewCurve, existingCurve);
+            bool areCloseEnough = AreCloseEnough(firstNewCurve, secondNewCurve, existingCurve);
+            bool areAlmostParallel = AreAlmostParallel(firstNewCurve, secondNewCurve, existingCurve);
+
+            return areAlmostColinear && areCloseEnough;
         }
 
-        private double ProximityMeasure(FeatureCurve featureCurve, FeatureCurve featureCurve_2, FeatureCurve featureCurve_3)
+        private bool AreAlmostParallel(FeatureCurve firstNewCurve, FeatureCurve secondNewCurve, FeatureCurve existingCurve)
         {
-            throw new NotImplementedException();
+            var firstCrossLength = Vector3D.CrossProduct(firstNewCurve.NormalResult, secondNewCurve.NormalResult).Length;
+            var secondCrossLength = Vector3D.CrossProduct(firstNewCurve.NormalResult, existingCurve.NormalResult).Length;
+            var thirdCrossLength = Vector3D.CrossProduct(secondNewCurve.NormalResult, existingCurve.NormalResult).Length;
+
+            var maxCross = Math.Max(firstCrossLength, Math.Max(secondCrossLength, thirdCrossLength));
+            var angle = Math.Acos(maxCross);
+
+            return Math.Abs(angle) <= parallelismThreshold;
+        }
+
+        private bool AreCloseEnough(FeatureCurve firstNewCurve, FeatureCurve secondNewCurve, FeatureCurve existingCurve)
+        {
+            return ProximityMeasure(firstNewCurve, secondNewCurve, existingCurve) <= proximityThreshold;
+        }
+
+        private bool AreAlmostColinear(FeatureCurve firstNewCurve, FeatureCurve secondNewCurve, FeatureCurve existingCurve)
+        {
+            double[] angles = { GetAngle(firstNewCurve, secondNewCurve, existingCurve),
+                                GetAngle(firstNewCurve, existingCurve, secondNewCurve),
+                                GetAngle(secondNewCurve, firstNewCurve, existingCurve) };
+            var minAngle = angles.Min();
+            return minAngle < colinearityThreshold;
+        }
+
+        private double GetAngle(FeatureCurve firstNewCurve, FeatureCurve secondNewCurve, FeatureCurve existingCurve)
+        {
+            var c1 = firstNewCurve.CenterResult;
+            var c2 = secondNewCurve.CenterResult;
+            var c3 = existingCurve.CenterResult;
+
+            var u = c1 - c2;
+            var v = c3 - c1;
+
+            var cos = Vector3D.DotProduct(u, v) / (u.Length * v.Length);
+            var angle = Math.Acos(cos);
+
+            return angle;
+        }
+
+        private double ProximityMeasure(FeatureCurve firstNewCurve, FeatureCurve secondNewCurve, FeatureCurve existingCurve)
+        {
+            var d1 = (existingCurve.CenterResult - firstNewCurve.CenterResult).Length;
+            var d2 = (existingCurve.CenterResult - secondNewCurve.CenterResult).Length;
+            return Math.Min(d1, d2);
         }
     }
 }
