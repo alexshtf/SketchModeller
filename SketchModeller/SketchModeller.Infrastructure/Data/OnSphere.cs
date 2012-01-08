82eaa5dd92422c58ed2d1f1a381500a28bedd80e
Alex <alex.shtf@gmail.com>
2012-01-08 11:29:41 +0200

    Added on-sphere annotation to infrastructure
    reference #1

---
+++ SketchModeller/SketchModeller.Infrastructure/Data/OnSphere.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SketchModeller.Infrastructure.Data
+{
+    [Serializable]
+    public class OnSphere : Annotation
+    {
+        public FeatureCurve SphereOwned { get; set; }
+        public FeatureCurve CenterTouchesSphere { get; set; }
+    }
+}
