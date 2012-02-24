5e2c8035a460a3b9a95fd7f4c62d033511a502a3
Alex <alex.shtf@gmail.com>
2012-02-24 14:32:36 +0200

    Added a constrained solver based on a constant penalty method.

---
+++ SketchModeller/SketchModeller.Utilities/Optimization/ConstantPenaltySolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoDiff;
+
+namespace SketchModeller.Utilities.Optimization
+{
+    public class ConstantPenaltySolver : IConstrainedSolver
+    {
+        private readonly double penaltyWeight;
+        private readonly double gradientNormThreshold;
+        private readonly IFirstOrderUnconstrainedOptimizer unconstrainedOptimizer;
+
+        public ConstantPenaltySolver(double penaltyWeight, double gradientNormThreshold, IFirstOrderUnconstrainedOptimizer unconstrainedOptimizer)
+        {
+            this.penaltyWeight = penaltyWeight;
+            this.gradientNormThreshold = gradientNormThreshold;
+            this.unconstrainedOptimizer = unconstrainedOptimizer;
+        }
+
+        public IEnumerable<double[]> Solve(Term objective, IEnumerable<Term> constraints, Variable[] variables, double[] startPoint)
+        {
+            var penalizedObjective = objective + penaltyWeight * TermUtils.SafeSum(constraints);
+            var compiledPenalizedObjective = penalizedObjective.Compile(variables);
+            var solution = unconstrainedOptimizer.Solve(x => compiledPenalizedObjective.Differentiate(x), startPoint, gradientNormThreshold);
+            yield return solution;
+        }
+    }
+}
