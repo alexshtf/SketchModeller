5682f38f364d00e8b77502aa838ea0b46de60498
Alex <alex.shtf@gmail.com>
2012-02-24 19:00:50 +0200

    Added multistage constrained solver - The multistage constrained solver will produce all solutions of all its stages.

---
+++ SketchModeller/SketchModeller.Utilities/Optimization/MultistageConstrainedOptimizer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoDiff;
+
+namespace SketchModeller.Utilities.Optimization
+{
+    class MultistageConstrainedOptimizer : IConstrainedSolver
+    {
+        private readonly IConstrainedSolver[] stages;
+
+        public MultistageConstrainedOptimizer(params IConstrainedSolver[] stages)
+        {
+            this.stages = stages.ToArray();
+        }
+
+        public IEnumerable<double[]> Solve(Term objective, IEnumerable<Term> constraints, Variable[] variables, double[] startPoint)
+        {
+            double[] lastSolution = startPoint;
+            foreach (var stage in stages)
+            {
+                var solutions = stage.Solve(objective, constraints, variables, lastSolution);
+                foreach (var solution in solutions)
+                {
+                    yield return solution;
+                    lastSolution = solution;
+                }
+            }
+        }
+    }
+}
