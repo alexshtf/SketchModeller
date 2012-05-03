84b6e5474a8b613212974dcca95a1bc5cad5ccc2
Alex <alex.shtf@gmail.com>
2012-05-03 12:03:50 +0300

    Fixed rectangle feature curve - width/height are scalars, not vectors

--- SketchModeller/SketchModeller.Infrastructure/Data/RectangleFeatureCurve.cs
+++ SketchModeller/SketchModeller.Infrastructure/Data/RectangleFeatureCurve.cs
@@ -9,9 +9,24 @@ namespace SketchModeller.Infrastructure.Data
     [Serializable]
     public class RectangleFeatureCurve : FeatureCurve
     {
-        public TVec Widgth { get; set; }
-        public TVec Height { get; set; }
+        /// <summary>
+        /// A term for the rectangle's width
+        /// </summary>
+        public Term Widgth { get; set; }
+
+        /// <summary>
+        /// A term for the rectangle's height
+        /// </summary>
+        public Term Height { get; set; }
+
+        /// <summary>
+        /// The optimized rectangle width
+        /// </summary>
         public double WidthResult { get; set; }
+
+        /// <summary>
+        /// The optimized rectangle height
+        /// </summary>
         public double HeightResult { get; set; }
     }
 }
