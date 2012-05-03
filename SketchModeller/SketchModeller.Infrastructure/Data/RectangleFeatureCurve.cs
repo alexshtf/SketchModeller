09c97f72381204e18b5ccdca75a8d39f3577ef87
Alex <alex.shtf@gmail.com>
2012-05-03 12:02:18 +0300

    Added a rectangular feature curve class

---
+++ SketchModeller/SketchModeller.Infrastructure/Data/RectangleFeatureCurve.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoDiff;
+
+namespace SketchModeller.Infrastructure.Data
+{
+    [Serializable]
+    public class RectangleFeatureCurve : FeatureCurve
+    {
+        public TVec Widgth { get; set; }
+        public TVec Height { get; set; }
+        public double WidthResult { get; set; }
+        public double HeightResult { get; set; }
+    }
+}
