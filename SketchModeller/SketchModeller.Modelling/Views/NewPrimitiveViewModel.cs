04515abfb4dd055586996f3404b409a1e2ddfbbc
Alex <alex.shtf@gmail.com>
2012-05-13 11:01:53 +0300

    Imported NewPrimitiveViewModel from Alexander's solution

--- SketchModeller/SketchModeller.Modelling/Views/NewPrimitiveViewModel.cs
+++ SketchModeller/SketchModeller.Modelling/Views/NewPrimitiveViewModel.cs
@@ -43,7 +43,7 @@ namespace SketchModeller.Modelling.Views
 
         public ObservableCollection<MenuCommandData> ContextMenu { get; private set; }
 
-        public NewPrimitive Model 
+        public NewPrimitive Model
         {
             get { return model; }
             set
@@ -80,12 +80,12 @@ namespace SketchModeller.Modelling.Views
         /// <returns></returns>
         public abstract IEditor StartEdit(Point startPos, LineRange startRay);
 
-        #region IEditable implementation 
+        #region IEditable implementation
 
         public abstract Vector3D ApproximateAxis { get; }
-        
+
         public SketchPlane SketchPlane { get { return uiState.SketchPlane; } }
-        
+
         public void NotifyDragged()
         {
             ApplyConstraints();
@@ -94,7 +94,7 @@ namespace SketchModeller.Modelling.Views
             ComputeCurvesAssignment();
             eventAggregator.GetEvent<PrimitiveCurvesChangedEvent>().Publish(Model);
         }
-        
+
         #endregion
 
         private void ApplyConstraints()
@@ -125,39 +125,83 @@ namespace SketchModeller.Modelling.Views
         {
             if (Model.IsSelected)
                 Model.ClearColorCodingFromSketch();
-            //Compute at least one silhouette curve
-            curveAssigner.ComputeSilhouetteAssignments(Model);
-            //Pick the first available silhouette Curve
-            Model.SilhouetteCurves[0].isDeselected = true;
-            Model.SilhouetteCurves[0].AssignedTo.isdeselected = true;
-            //Unassing the second silhouette curve if it exists
-            if (Model.SilhouetteCurves.Length > 1 && Model.SilhouetteCurves[1].AssignedTo != null)
-                Model.SilhouetteCurves[1].AssignedTo = null;
-            //Find all compatible feature curve
-            while (curveAssigner.ComputeFeatureAssignments(Model))
-                Model.CheckFeatureCurves();
-            var NumberOfActiveFeatureCurves = Model.FeatureCurves
-                .Select(c => c)
-                .Where(c => c.AssignedTo != null)
-                .ToArray().Length;
-            if (NumberOfActiveFeatureCurves > 0)
-                while (curveAssigner.ComputeSilhouetteAssignments(Model))
-                {
-                    if (NumberOfActiveFeatureCurves == 1) Model.CheckSilhoueteCurveWith1Feature();
-                    else Model.CheckSilhoueteCurveWith2Features();
-                }
-            var NumberOfActiveSilhouettes= Model.SilhouetteCurves
-                .Select(c => c)
-                .Where(c => c.AssignedTo != null)
-                .ToArray().Length;
-            if (NumberOfActiveSilhouettes > 1)
+            if (Model.GetType() != typeof(NewCuboid))
+            {
+                //Compute at least one silhouette curve
+                curveAssigner.ComputeSilhouetteAssignments(Model);
+                //Pick the first available silhouette Curve
+                Model.SilhouetteCurves[0].isDeselected = true;
+                Model.SilhouetteCurves[0].AssignedTo.isdeselected = true;
+                //Unassing the second silhouette curve if it exists
+                if (Model.SilhouetteCurves.Length > 1 && Model.SilhouetteCurves[1].AssignedTo != null)
+                    Model.SilhouetteCurves[1].AssignedTo = null;
+                //Find all compatible feature curve
+                while (curveAssigner.ComputeFeatureAssignments(Model))
+                    Model.CheckFeatureCurves();
+                var NumberOfActiveFeatureCurves = Model.FeatureCurves
+                    .Select(c => c)
+                    .Where(c => c.AssignedTo != null)
+                    .ToArray().Length;
                 if (NumberOfActiveFeatureCurves > 0)
-                    Model.ValidateFeatureCurves();
+                    while (curveAssigner.ComputeSilhouetteAssignments(Model))
+                    {
+                        if (NumberOfActiveFeatureCurves == 1) Model.CheckSilhoueteCurveWith1Feature();
+                        else Model.CheckSilhoueteCurveWith2Features();
+                    }
+                var NumberOfActiveSilhouettes = Model.SilhouetteCurves
+                    .Select(c => c)
+                    .Where(c => c.AssignedTo != null)
+                    .ToArray().Length;
+                if (NumberOfActiveSilhouettes > 1)
+                    if (NumberOfActiveFeatureCurves > 0)
+                        Model.ValidateFeatureCurves();
+            }
+            else
+            {
+                NewCuboid cuboid = (NewCuboid)Model;
+                List<Tuple<int, Point, Point>> cornerList = new List<Tuple<int, Point, Point>>();
+                for (int i = 0; i < cuboid.ArrayOfCorners.Length; i++)
+                {
+                    while (curveAssigner.ComputeFeatureAssignments(cuboid.ArrayOfCorners[i]))
+                    {
+                        Model.CheckCubicCorner(i);
+
+                        int NumberOfAssignedFeatures = (from curve in cuboid.ArrayOfCorners[i]
+                                                        where curve.AssignedTo != null
+                                                        select curve).Count();
+                        if (NumberOfAssignedFeatures == 3)
+                        {
+                            Point CP = NewPrimitiveExtensions.FindCoincidentPoint(cuboid.ArrayOfCorners[i]);
+                            Point CPprimitive = NewPrimitiveExtensions.FindCoincidentPoint(cuboid.ArrayOfCorners[i], false);
+                            cornerList.Add(Tuple.Create(i, CP, CPprimitive));
+                        }
+                    }
+                    curveAssigner.refresh(Model);
+                    cuboid.ArrayOfCorners[i][0].AssignedTo = null;
+                    cuboid.ArrayOfCorners[i][1].AssignedTo = null;
+                    cuboid.ArrayOfCorners[i][2].AssignedTo = null;
+                }
+
+                double minDistance = 10e10;
+                foreach (var cubicTuple in cornerList)
+                {
+                    Vector v = cubicTuple.Item2 - cubicTuple.Item3;
+                    if (v.Length < minDistance)
+                    {
+                        cuboid.ActiveCubicCorner = cubicTuple.Item1;
+                        minDistance = v.Length;
+                    }
+                }
+                while (curveAssigner.ComputeFeatureAssignments(cuboid.ArrayOfCorners[cuboid.ActiveCubicCorner]))
+                {
+                    Model.CheckCubicCorner(cuboid.ActiveCubicCorner);
+                }
+            }
             //refresh all curves so that they can be selected again
             curveAssigner.refresh(Model);
             Model.CanSnap = false;
-            Model.CanSnap = CheckIfPrimitiveCanSnap(); 
-            
+            Model.CanSnap = CheckIfPrimitiveCanSnap();
+
             if (Model.IsSelected)
                 Model.SetColorCodingToSketch();
         }
@@ -165,14 +209,15 @@ namespace SketchModeller.Modelling.Views
         public bool CheckIfPrimitiveCanSnap()
         {
             bool ModelCanSnap = false;
-            if (Model.GetType() == typeof(NewSphere)) {
+            if (Model.GetType() == typeof(NewSphere))
+            {
                 if (Model.SilhouetteCurves.Length > 0)
                     ModelCanSnap = true;
             }
             if (Model.GetType() == typeof(NewCylinder))
             {
                 if (Model.SilhouetteCurves.Length > 1)
-                    if (Model.SilhouetteCurves[0].AssignedTo != null && Model.SilhouetteCurves[1].AssignedTo != null) 
+                    if (Model.SilhouetteCurves[0].AssignedTo != null && Model.SilhouetteCurves[1].AssignedTo != null)
                         ModelCanSnap = true;
             }
             if (Model.GetType() == typeof(NewCone))
@@ -193,10 +238,24 @@ namespace SketchModeller.Modelling.Views
                     if (Model.SilhouetteCurves[0].AssignedTo != null && Model.SilhouetteCurves[1].AssignedTo != null
                         && Model.FeatureCurves[0].AssignedTo != null && Model.FeatureCurves[1].AssignedTo != null)
                     {
-
                         ModelCanSnap = true;
                     }
             }
+            if (Model.GetType() == typeof(NewCuboid))
+            {
+                NewCuboid CuboidModel = (NewCuboid)Model;
+                /*int TotalMatched = 
+                    (from curve in CuboidModel.LUFcubicCorner
+                    where (curve.AssignedTo != null)
+                    select curve
+                    ).ToArray().Length;
+
+                if (TotalMatched == 3) ModelCanSnap = true;*/
+                if (CuboidModel.ActiveCubicCorner >= 0)
+                    ModelCanSnap = true;
+                Debug.WriteLine("Can Snap:{0}", ModelCanSnap);
+            }
+
             return ModelCanSnap;
         }
 
