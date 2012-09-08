3ea5d193790680f315d94584ee9183af9d4a5042
Alex <alex.shtf@gmail.com>
2012-09-08 13:55:47 +0300

    Added undo event to infrastructure

---
+++ SketchModeller/SketchModeller.Infrastructure/Services/IUndoHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reactive;
+
+namespace SketchModeller.Infrastructure.Services
+{
+    public interface IUndoHistory
+    {
+        void Push();
+        void Pop();
+    }
+}
