7601aa77ea1f50b0bd32f522238e68f698b56926
Alex <alex.shtf@gmail.com>
2012-09-08 19:48:25 +0300

    Classification graph construction performance

--- SketchModeller/SketchModeller.Modelling/Services/ClassificationInference/GraphConstructor.cs
+++ SketchModeller/SketchModeller.Modelling/Services/ClassificationInference/GraphConstructor.cs
@@ -25,13 +25,19 @@ namespace SketchModeller.Modelling.Services.ClassificationInference
                 select new Node(sketchCurve, geometricClass);
             var nodes = nodesQuer.ToArray();
 
+            var allPairs = from first in nodes
+                           from second in nodes
+                           select Tuple.Create(first, second);
+            allPairs = allPairs.ToArray();
+
             var neighbors =
-                from first in nodes
-                from second in nodes
+                from pair in allPairs.AsParallel()
+                let first = pair.Item1
+                let second = pair.Item2
                 where AreNeighbors(first, second)
                 select Tuple.Create(first, second);
 
-            foreach (var pair in neighbors)
+            foreach (var pair in neighbors.ToArray())
             {
                 pair.Item1.Neighbors.Add(pair.Item2);
                 pair.Item2.Neighbors.Add(pair.Item1);
